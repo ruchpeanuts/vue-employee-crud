2da2bd1072aec22639d67bdd2d11ec4d2cf7bcb7
Rucha Dalsania <dalsania.rucha@yahoo.co.in>
2020-10-07 22:44:09 -0400

    fixed package json and add/list employee

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,82 @@
 <template>
-  <b-container >
-    <p>Hello, Bootstrap-Vue</p>
-
-    <employee-table :employees="employees" />
-
-<b-form>
-    <b-row>
-      <b-col>
-        <label>Employee Id-number</label>
-      </b-col>
-      <b-col>
-        <input v-model="newEmp.id"/>
-        </b-col>
-    </b-row>
-    <b-row>
-      <b-col>
-        <label>Employee Name</label>
-        </b-col>
-        <b-col>
-        <input type="text" placeholder="snoopy"
-        v-model="newEmp.name"/>
-        </b-col>
-    </b-row>
-    <div class="float-md-right">
-      <b-btn variant="primary" @click="saveEmployee">
+  <b-container>
+    <div>
+      <b-btn
+        class="float-md-right"
+        variant="primary"
+        @click="state = 'editEmployee'"
+        v-show="state === 'default'"
+      >
         Add Employee
       </b-btn>
 
+      <div v-show="state === 'editEmployee'">
+        <b-form>
+          <b-row>
+            <b-col>
+              <label>Employee Id-number</label>
+            </b-col>
+            <b-col>
+              <input type="Number" v-model="newEmp.id" />
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <label>Employee Name</label>
+            </b-col>
+            <b-col>
+              <input type="text" placeholder="snoopy" v-model="newEmp.name" />
+            </b-col>
+          </b-row>
+          <div class="float-md-right">
+            <b-btn variant="primary" @click="saveEmployee">
+              Save Employee
+            </b-btn>
+          </div>
+        </b-form>
+      </div>
     </div>
-    
-    
-</b-form>
-
+    <employee-table :employees="employees" />
+    <!-- 
+     style="background-image: url('https://picsum.photos/1140/1140');"
+     -->
   </b-container>
-
-
-
 </template>
 <script>
-import EmployeeTable from '@/components/EmployeeTable.vue'
+import EmployeeTable from "@/components/EmployeeTable.vue";
 
 export default {
-  components : {
-    EmployeeTable
+  components: {
+    EmployeeTable,
   },
-  methods:{
-    saveEmployee:function(){
-    this.employees.push({ id: this.newEmp.id, name: this.newEmp.name })
-    }
+  methods: {
+    saveEmployee: function() {
+      this.employees.push({ id: this.newEmp.id, name: this.newEmp.name });
+    },
   },
   data() {
-    return{
+    return {
+      state: "default",
       showAlert: false,
-      newEmp:{
-        id:'',
-        name:''
+      newEmp: {
+        id: "",
+        name: "",
       },
-      employees:[
+      employees: [
         {
-          id:1,
-          name:'big boo'
+          id: 1,
+          name: "big boo",
         },
         {
-          id:2,
-          name:'small boo'
+          id: 2,
+          name: "small boo",
         },
         {
-          id:3,
-          name:'peanutes'
-        }
-      ]
-    }
-  }
+          id: 3,
+          name: "peanutes",
+        },
+      ],
+    };
+  },
 };
 </script>
-
-
--- src/components/EmployeeTable.vue
+++ src/components/EmployeeTable.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="employee-table">
-    
-    <b-table striped hover :items="employees"></b-table>
+    <b-table
+      striped
+      hover
+      bordered
+      fixed
+      responsive
+      :items="employees"
+    ></b-table>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'employee-table',
-    props: {
+export default {
+  name: "employee-table",
+  props: {
     employees: Array,
-    },
-     
-  }
+  },
+};
 </script>
 
 <style scoped></style>
